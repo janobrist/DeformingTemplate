2af2198b64612f0a84f51f5681f49b99f1140bc3
PokingRobot <jobrist@ethz.ch>
2024-05-30 06:20:27 +0200

    update

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", type=int, default=8)
     parser.add_argument("--epochs", type=int, default=500)
     parser.add_argument("--lr", type=float, default=1e-4)
-    parser.add_argument("--datasets", type=list, default=["Octopus"])
-    parser.add_argument("--cameras", type=list, default=["0068"])
+    parser.add_argument("--datasets", type=list, default=["Paper"])
+    parser.add_argument("--cameras", type=list, default=["0005", "0029" ,"0068", "0089"])
     parser.add_argument("--force_features", type=bool, default=False)
 
     parser.add_argument("--chamfer_weight", type=float, default=1.0)
